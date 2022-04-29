3679f0e51dfa68ff2702dd65398add98d3bd9bc7
Josh Clyde <joshclyde11@gmail.com>
2022-04-28 20:13:08 -0400

    Add a global CSS file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import {defineCustomElements} from '../loader';
+import '../src/global.css'
+
+defineCustomElements();
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +10,18 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#1a1a1a',
+      },
+      {
+        name: 'light',
+        value: '#ebebeb',
+      },
+    ],
   },
 }
